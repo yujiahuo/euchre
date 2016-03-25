b44d9b1f9ea58febe7810d47b40ffbfe6dfe9ded
Yujia Huo <yujiahuo7@gmail.com>
2016-03-25 08:29:04 -0500

    Settings menu more fleshed out

--- gameLogic.js
+++ gameLogic.js
@@ -5,5 +5,33 @@ function newGame(){
 }
 
 function settings(){
-	
+	disappearMenu("start");
+	appearMenu("settings");
+}
+
+function howToPlay(){
+	document.getElementById("menu").classList.remove("startMenu");
+	document.getElementById("menu").classList.add("howToMenu");
+	document.getElementById("startMenuItems").style.opacity = "0";
+}
+
+function appearMenu(menuName){
+	document.getElementById("menu").classList.add(menuName+"Active");
+	document.getElementById(menuName+"MenuItems").style.opacity = "1";
+	toggleMenu(menuName+"MenuItems", 1);
+}
+
+function disappearMenu(menuName){
+	document.getElementById("menu").classList.remove(menuName+"Active");
+	document.getElementById(menuName+"MenuItems").style.opacity = "0";
+	setTimeout(toggleMenu, 300, menuName+"MenuItems", 0);
+}
+
+function toggleMenu(elemID, on){
+	if(on){
+		document.getElementById(elemID).style.display = "inline";
+	}
+	else{
+		document.getElementById(elemID).style.display = "none";
+	}
 }

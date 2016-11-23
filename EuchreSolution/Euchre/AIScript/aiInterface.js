6c7e3ae9bdf2ece32df78921060452ca0a675bd3
Kevin Lyles <kevinlyles@gmail.com>
2016-11-23 12:32:47 -0600

    Removed generated and obsolete files, added to .gitignore

--- EuchreSolution/Euchre/AIScript/aiInterface.js
+++
@@ -1,6 +0,0 @@
-/******************************************************
-/* Functions AIs have to implement
-/*
-/* You may call functions from playerAPI.js
-/* All other functions and vars are "private"
-*******************************************************/

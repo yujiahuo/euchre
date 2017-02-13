yujiahuo/euchre

commit 722b04e55ded3dd99d02dae9cd35b146f3622044

Updated to use strict null checking, fixed all compilation issues that arose

EuchreSolution/Euchre/GameScript/animation.ts
@@ -15,7 +15,8 @@ function makeCardElem(cardID: string, flippedUp: boolean): HTMLDivElement {
 		card.classList.add("cardBack");
 	}
 
-	document.getElementById("cardsContainer").appendChild(card);
+	let cardsContainer = document.getElementById("cardsContainer") as HTMLElement;
+	cardsContainer.appendChild(card);
 
 	card.style.zIndex = zIndex;
 	zIndex++;
@@ -44,13 +45,14 @@ function animDeal(hands: Card[][]): void {
 	let cardID: string;
 	let flippedUp: boolean;
 	let dealer: Player;
+	let trumpCandidateCard = game.getTrumpCandidateCard() as Card
 
 	dealer = game.getDealer();
 	player = nextPlayer(dealer);
 	delay = 0;
 
 	makeCardElem("deck", false);
-	makeCardElem(game.getTrumpCandidateCard().id, false);
+	makeCardElem(trumpCandidateCard.id, false);
 
 	for (let i = 0; i < hands.length; i++) {
 		flippedUp = (!game.getAIPlayer(player) || game.isOpenHands());
@@ -77,7 +79,7 @@ function animDeal(hands: Card[][]): void {
 		player = (player + 1) % 4;
 	}
 
-	setTimeout(animFlipCard, 1000, game.getTrumpCandidateCard().id);
+	setTimeout(animFlipCard, 1000, trumpCandidateCard.id);
 }
 
 function animDealSingle(player: Player, cardID: string, cardPos: number): void {
@@ -142,7 +144,8 @@ function animPlaceDealerButt(): void {
 	if (button === null) {
 		button = document.createElement("div");
 		button.id = "dealerButton";
-		document.getElementById("gameSpace").appendChild(button);
+		let gameSpace = document.getElementById("gameSpace") as HTMLElement;
+		gameSpace.appendChild(button);
 	}
 	switch (game.getDealer()) {
 		case Player.South:
@@ -207,8 +210,8 @@ function animSortHand(hand: Card[]): void {
 function animPlayCard(player: Player, cardID: string, flipCard: boolean): void {
 	if (game.isStatMode()) return;
 
-	let top: string;
-	let left: string;
+	let top = "";
+	let left = "";
 
 	if (flipCard && !game.isOpenHands()) animFlipCard(cardID);
 
@@ -238,7 +241,10 @@ function animPlayCard(player: Player, cardID: string, flipCard: boolean): void {
 function animFlipCard(cardID: string): void {
 	if (game.isStatMode()) return;
 
-	document.getElementById(cardID).classList.toggle("cardBack");
+	let cardElement = document.getElementById(cardID);
+	if (cardElement) {
+		cardElement.classList.toggle("cardBack");
+	}
 }
 
 function animWinTrick(player: Player, cards: Card[]): void {
@@ -294,12 +300,12 @@ function animRemoveKitty(): void {
 	}
 }
 
-function animHidePartnerHand(hands: Card[][]): void {
+function animHidePartnerHand(alonePlayer: Player, hands: Card[][]): void {
 	if (game.isStatMode()) return;
 
-	let player = getPartner(game.getAlonePlayer());
+	let player = getPartner(alonePlayer);
 	for (let i = 0; i < hands[player].length; i++) {
-		animHideCard(document.getElementById(hands[player][i].id));
+		animHideCard(document.getElementById(hands[player][i].id) as HTMLElement);
 	}
 }
 
@@ -311,9 +317,14 @@ function animHideCard(cardElem: HTMLElement): void {
 
 function animClearTable(): void {
 	if (game.isStatMode()) return;
-	document.getElementById("cardsContainer").innerHTML = "";
+
+	let cardsContainer = document.getElementById("cardsContainer")
+	if (cardsContainer) {
+		cardsContainer.innerHTML = "";
+	}
 }
 
+/*
 //let human player poke the buttons
 function animEnableBidding(hand: Card[]): void {
 	if (game.isStatMode()) return;
@@ -360,6 +371,7 @@ function animDisableBidding(): void {
 	document.getElementById("alone").style.backgroundColor = "green";
 }
 
+
 //flips a button on or off
 //needs to be generic but for now flips the 'go alone' button
 function animFlipButton(on: boolean): void {
@@ -372,6 +384,7 @@ function animFlipButton(on: boolean): void {
 		document.getElementById("alone").style.backgroundColor = "green";
 	}
 }
+*/
 
 function animShowText(text: string, messageLevel: MessageLevel, nest?: number, overwrite?: boolean): void {
 	let allowedLevel: MessageLevel = game.getMessageLevel();
@@ -400,6 +413,9 @@ function animShowText(text: string, messageLevel: MessageLevel, nest?: number, o
 }
 function updateLog(text: string, overwrite?: boolean): void {
 	let div = document.getElementById("sidebarText");
+	if (!div) {
+		return;
+	}
 	if (overwrite) {
 		div.innerHTML = text;
 	} else {
@@ -410,6 +426,9 @@ function updateLog(text: string, overwrite?: boolean): void {
 
 function animShowTextTop(text: string, overwrite?: boolean): void {
 	let div = document.getElementById("sidebarTop");
+	if (!div) {
+		return;
+	}
 	if (overwrite) {
 		div.innerHTML = "";
 	}
@@ -417,9 +436,15 @@ function animShowTextTop(text: string, overwrite?: boolean): void {
 }
 
 function disableActions(): void {
-	document.getElementById("blanket").style.display = "inline";
+	let blanket = document.getElementById("blanket");
+	if (blanket) {
+		blanket.style.display = "inline";
+	}
 }
 
 function enableActions(): void {
-	document.getElementById("blanket").style.display = "none";
+	let blanket = document.getElementById("blanket");
+	if (blanket) {
+		blanket.style.display = "none";
+	}
 }

EuchreSolution/Euchre/GameScript/game.ts
@@ -23,11 +23,11 @@ class Game {
 	private __gameStage: GameStage; //bidding round 1, bidding round 2, or trick playing
 	private __playersBid: number; //number of players who have bid so far
 	private __hands: Card[][]; //2d array of everyone's hands
-	private __trumpCandidateCard: Card; //turned up card
-	private __trumpSuit: Suit;	//current trump suit
+	private __trumpCandidateCard: Card | undefined; //turned up card
+	private __trumpSuit: Suit | undefined;	//current trump suit
 	private __dealer: Player;
-	private __maker: Player; //player who called trump
-	private __alonePlayer: Player;
+	private __maker: Player | undefined; //player who called trump
+	private __alonePlayer: Player | undefined;
 	private __numPlayers: number; //players playing this hand; this is usually 4 but can be 3 or 2 depending on loners
 	private __nsTricksWon: number;
 	private __ewTricksWon: number;
@@ -44,7 +44,7 @@ class Game {
 	private __showTrickHistory: boolean;
 	private __statMode: boolean;
 	private __messageLevel: MessageLevel;
-	private __aiPlayers: EuchreAI[];
+	private __aiPlayers: (EuchreAI | null)[];
 	private __hasHooman: boolean; //if there is a human player
 	//#endregion
 
@@ -63,19 +63,22 @@ class Game {
 	public getEwScore(): number {
 		return this.__ewScore;
 	}
-	public getTrumpCandidateCard(): Card {
+	public getTrumpCandidateCard(): Card | undefined {
+		if (!this.__trumpCandidateCard) {
+			return;
+		}
 		return new Card(this.__trumpCandidateCard.suit, this.__trumpCandidateCard.rank);
 	}
-	public getTrumpSuit(): Suit {
+	public getTrumpSuit(): Suit | undefined {
 		return this.__trumpSuit;
 	}
 	public getDealer(): Player {
 		return this.__dealer;
 	}
-	public getMaker(): Player {
+	public getMaker(): Player | undefined {
 		return this.__maker;
 	}
-	public getAlonePlayer(): Player {
+	public getAlonePlayer(): Player | undefined {
 		return this.__alonePlayer;
 	}
 	public getNumPlayers(): number {
@@ -84,10 +87,10 @@ class Game {
 	public getTrickNum(): number {
 		return this.__trickNum;
 	}
-	public getTrickPlayersPlayed(): number {
-		if(this.__trick) return this.__trick.playersPlayed();
+	public getTrickPlayersPlayed(): number | undefined {
+		if (this.__trick) return this.__trick.playersPlayed();
 	}
-	public getTrickSuit(): Suit {
+	public getTrickSuit(): Suit | undefined {
 		if (this.__trick) return this.__trick.suitLead();
 	}
 	public getTrickPlayedCards(): PlayedCard[] {
@@ -95,7 +98,7 @@ class Game {
 		let card: Card;
 		let cardCopy: Card;
 
-		if (!this.__trick) return null;
+		if (!this.__trick) return [];
 
 		for (let i = 0; i < this.__trick.cardsPlayed().length; i++) {
 			card = this.__trick.cardsPlayed()[i].card;
@@ -124,11 +127,11 @@ class Game {
 	public getMessageLevel(): MessageLevel {
 		return this.__messageLevel;
 	}
-	public getAIPlayer(player: Player): EuchreAI {
+	public getAIPlayer(player: Player): EuchreAI | null {
 		return (this.__aiPlayers[player]);
 	}
 	public myHand(): Card[] {
-		let hand = [];
+		let hand: Card[] = [];
 		let card;
 
 		for (let i = 0; i < this.__hands[this.__currentPlayer].length; i++) {
@@ -151,8 +154,8 @@ class Game {
 		this.initPlay();
 		while (playGame) {
 			this.doStep();
-			if (this.__gameStage === null) {
-				playGame = false
+			if (this.__gameStage === GameStage.OutsideGame) {
+				playGame = false;
 			}
 		}
 		if (this.isStatMode()) {
@@ -174,6 +177,8 @@ class Game {
 	private letHumanClickCards(): void { }
 
 	private doStep(): void {
+		let aiPlayer: EuchreAI | null;
+
 		animShowText("STAGE: " + GameStage[this.__gameStage], MessageLevel.Step);
 		switch (this.__gameStage) {
 			case GameStage.NewGame:
@@ -183,33 +188,37 @@ class Game {
 				this.initHand();
 				break;
 			case GameStage.BidRound1:
-				if (!this.__aiPlayers[this.__currentPlayer]) {
+				aiPlayer = this.__aiPlayers[this.__currentPlayer];
+				if (!aiPlayer) {
 					this.letHumanBid(1);
 					return;
 				}
-				this.handleBid();
+				this.handleBid(aiPlayer);
 				break;
 			case GameStage.BidRound2:
-				if (!this.__aiPlayers[this.__currentPlayer]) {
+				aiPlayer = this.__aiPlayers[this.__currentPlayer];
+				if (!aiPlayer) {
 					this.letHumanBid(2);
 					return;
 				}
-				this.handleBid();
+				this.handleBid(aiPlayer);
 				break;
 			case GameStage.Discard:
 				this.__currentPlayer = this.__dealer;
-				if (!this.__aiPlayers[this.__dealer]) {
+				aiPlayer = this.__aiPlayers[this.__dealer];
+				if (!aiPlayer) {
 					this.letHumanClickCards();
 					return;
 				}
-				this.discardCard(this.__aiPlayers[this.__dealer].pickDiscard());
+				this.discardCard(aiPlayer.pickDiscard());
 				break;
 			case GameStage.PlayTricks:
-				if (!this.__aiPlayers[this.__currentPlayer]) {
+				aiPlayer = this.__aiPlayers[this.__currentPlayer];
+				if (!aiPlayer) {
 					this.letHumanClickCards();
 					return;
 				}
-				if (this.__trick.isFinished()){
+				if (this.__trick.isFinished()) {
 					this.endTrick();
 				}
 				this.__trick.playTrickStep(this.__currentPlayer);
@@ -250,10 +259,10 @@ class Game {
 	//resets variables, gets dealer, shuffles deck, inits empty hands, sets currentplayer to left of dealer
 	private initHand(): void {
 		this.__playersBid = 0;
-		this.__trumpCandidateCard = null;
-		this.__trumpSuit = null;
-		this.__maker = null;
-		this.__alonePlayer = null;
+		this.__trumpCandidateCard = undefined;
+		this.__trumpSuit = undefined;
+		this.__maker = undefined;
+		this.__alonePlayer = undefined;
 		this.__numPlayers = 0;
 		this.__nsTricksWon = 0;
 		this.__ewTricksWon = 0;
@@ -277,8 +286,9 @@ class Game {
 		//let AIs initialize
 		for (let i = 0; i < 4; i++) {
 			this.__currentPlayer = i;
-			if (this.__aiPlayers[i] !== null) {
-				this.__aiPlayers[i].init();
+			let aiPlayer = this.__aiPlayers[i];
+			if (aiPlayer !== null) {
+				aiPlayer.init();
 			}
 		}
 
@@ -288,10 +298,9 @@ class Game {
 	//#endregion
 
 	//get a bid
-	private handleBid(): void {
+	private handleBid(aiPlayer: EuchreAI): void {
 		let suit;
 		let alone;
-		let aiPlayer = this.__aiPlayers[this.__currentPlayer];
 
 		//see if AI bids
 		suit = getAIBid(aiPlayer, this.__gameStage);
@@ -325,7 +334,7 @@ class Game {
 		this.setTrump(suit, this.__currentPlayer, alone);
 		//if round 1, dealer also needs to discard
 		if (this.__gameStage === GameStage.BidRound1) {
-			this.addToHand(this.__dealer, this.__trumpCandidateCard);
+			this.addToHand(this.__dealer, this.__trumpCandidateCard as Card);
 			this.__gameStage = GameStage.Discard;
 		}
 		else {
@@ -355,10 +364,10 @@ class Game {
 		}
 	}
 
-	private discardCard(toDiscard: Card): void {
+	private discardCard(toDiscard: Card | null): void {
 		let card: Card;
 
-		if (toDiscard === null || !isInHand(this.__hands[this.__dealer], toDiscard)) {
+		if (!toDiscard || !isInHand(this.__hands[this.__dealer], toDiscard)) {
 			card = this.__hands[this.__dealer][0];
 		}
 		else {
@@ -372,13 +381,15 @@ class Game {
 
 	private startTricks(): void {
 		this.__gameStage = GameStage.PlayTricks;
-		this.__trick = new Trick(this.__trumpSuit, (this.__alonePlayer !== null), this.__hands, this.__aiPlayers);
+		this.__trick = new Trick(this.__trumpSuit as Suit, (this.__alonePlayer !== null), this.__hands, this.__aiPlayers);
+
 	}
 
 	private endTrick(): void {
 		for (let i = 0; i < 4; i++) {
-			if (this.__aiPlayers[i] !== null) {
-				this.__aiPlayers[i].trickEnd();
+			let aiPlayer = this.__aiPlayers[i];
+			if (aiPlayer !== null) {
+				aiPlayer.trickEnd();
 			}
 		}
 		this.scoreTrick();
@@ -386,8 +397,8 @@ class Game {
 			this.endHand();
 		}
 		else {
-			this.__currentPlayer = this.__trick.winner();
-			this.__trick = new Trick(this.__trumpSuit, (this.__alonePlayer !== null), this.__hands, this.__aiPlayers);
+			this.__currentPlayer = this.__trick.winner() as Player;
+			this.__trick = new Trick(this.__trumpSuit as Suit, (this.__alonePlayer !== null), this.__hands, this.__aiPlayers);
 			this.__trickNum++;
 		}
 	}
@@ -404,7 +415,7 @@ class Game {
 	}
 
 	private endHand(): void {
-		this.resetJacks();
+		this.resetJacks(this.__trumpSuit as Suit);
 		this.updateScore();
 		if (this.__nsScore >= 10 || this.__ewScore >= 10) {
 			this.endGame();
@@ -414,14 +425,14 @@ class Game {
 		}
 	}
 
-	private resetJacks(): void {
+	private resetJacks(trumpSuit: Suit): void {
 		let rightID;
 		let leftID;
 
-		rightID = Suit[this.__trumpSuit] + Rank.Jack;
+		rightID = Suit[trumpSuit] + Rank.Jack;
 		DECKDICT[rightID].rank = Rank.Jack;
-		leftID = Suit[getOppositeSuit(this.__trumpSuit)] + Rank.Jack;
-		DECKDICT[leftID].suit = getOppositeSuit(this.__trumpSuit);
+		leftID = Suit[getOppositeSuit(trumpSuit)] + Rank.Jack;
+		DECKDICT[leftID].suit = getOppositeSuit(trumpSuit);
 		DECKDICT[leftID].rank = Rank.Jack;
 	}
 
@@ -453,7 +464,7 @@ class Game {
 			this.__gameStage = GameStage.NewGame;
 		}
 		else {
-			this.__gameStage = null;
+			this.__gameStage = GameStage.OutsideGame;
 			animShowText("Games won: " + this.__nsGamesWon + " : " + this.__ewGamesWon, MessageLevel.Multigame);
 			animShowText("Total score: " + this.__nsTotalScore + " : " + this.__ewTotalScore, MessageLevel.Multigame);
 		}

EuchreSolution/Euchre/AIScript/aiInterface.ts
@@ -19,12 +19,12 @@ interface EuchreAI {
 	//Bidding round 1, if trump is ordered up to you, pick a card to discard
 	//Params: none
 	//Returns: Card or null
-	pickDiscard(): Card;
+	pickDiscard(): Card | null;
 
 	//Bidding round 2, choose from the remaining suits or pass
 	//Params: none
 	//Returns: Suit or null
-	pickTrump(): Suit;
+	pickTrump(): Suit | null;
 
 	//Called at any bidding round after you've determined trump
 	//Return true if going alone
@@ -35,7 +35,7 @@ interface EuchreAI {
 	//Your turn to play a card
 	//Params: none
 	//Returns: Card or null
-	pickCard(): Card;
+	pickCard(): Card | null;
 
 	//Called at the end of each trick
 	//Params: An array of players and the cards they played, in play order

EuchreSolution/Euchre/AIScript/decentAI.ts
@@ -18,11 +18,15 @@ class DecentAI implements EuchreAI {
 
 	pickDiscard(): Card {
 		let trumpSuit = game.getTrumpSuit();
-		return getWorstCard(this.hand, null, trumpSuit);
+		return getWorstCard(this.hand, undefined, trumpSuit);
 	}
 
-	pickTrump(): Suit {
-		let trumpCandidateSuit = game.getTrumpCandidateCard().suit;
+	pickTrump(): Suit | null {
+		let trumpCandidate = game.getTrumpCandidateCard();
+		if (!trumpCandidate) {
+			return null;
+		}
+		let trumpCandidateSuit = trumpCandidate.suit;
 		if (trumpCandidateSuit !== Suit.Clubs) {
 			this.handStrength = this.calculateHandStrength(Suit.Clubs);
 			if (this.handStrength > 2) return Suit.Clubs;
@@ -51,16 +55,16 @@ class DecentAI implements EuchreAI {
 		return false;
 	}
 
-	pickCard(): Card {
+	pickCard(): Card | null {
 		let numPlayersPlayed;
 		let playedCards;
-		let lowestWinningCard = null;
+		let lowestWinningCard: Card | null = null;
 		let lowestWinningValue = 9999;
 		let winningValue = 0;
 		let value;
 		let i;
 		let trickSuit = game.getTrickSuit();
-		let trumpSuit = game.getTrumpSuit();
+		let trumpSuit = game.getTrumpSuit() as Suit;
 
 		this.hand = game.myHand(); //you need to do this or else
 
@@ -71,7 +75,8 @@ class DecentAI implements EuchreAI {
 
 		//Find currently winning value
 		playedCards = game.getTrickPlayedCards();
-		winningValue = getCardValue(getBestCardPlayed(playedCards, trumpSuit).card, trumpSuit);
+		let bestPlayedCard = getBestCardPlayed(playedCards, trumpSuit) as PlayedCard;
+		winningValue = getCardValue(bestPlayedCard.card, trumpSuit);
 
 		//If not last player, play the lowest card that can win
 		//If we can't win, then sluff
@@ -119,6 +124,4 @@ class DecentAI implements EuchreAI {
 		}
 		return false;
 	}
-
-
 }

EuchreSolution/Euchre/GameScript/globs.ts
@@ -24,7 +24,7 @@ enum Suit {
 	Spades,
 }
 
-let suitsArray: Suit[] = [Suit.Clubs, Suit.Diamonds, Suit.Hearts, Suit.Spades];
+let suitsArray = [Suit.Clubs, Suit.Diamonds, Suit.Hearts, Suit.Spades];
 
 enum Rank {
 	Nine = 9,
@@ -38,6 +38,7 @@ enum Rank {
 }
 
 enum GameStage {
+	OutsideGame,
 	NewGame,
 	NewHand,
 	BidRound1,
@@ -78,7 +79,7 @@ const DECKSIZE = 24;
 const SORTEDDECK: Card[] = buildSortedDeck();
 
 function buildSortedDeck(): Card[] {
-	let deck = [];
+	let deck: Card[] = [];
 	let ranks: Rank[] = [Rank.Nine, Rank.Ten, Rank.Jack, Rank.Queen, Rank.King, Rank.Ace];
 
 	for (let suit of suitsArray) {

EuchreSolution/Euchre/GameScript/menuLogic.ts
@@ -3,7 +3,10 @@
 ***********************************/
 
 function newGame(): void {
-	document.getElementById("menu").style.display = "none";
+	let menu = document.getElementById("menu");
+	if (menu) {
+		menu.style.display = "none";
+	}
 	game = new Game();
 	game.start();
 }
@@ -33,31 +36,48 @@ function howToPlay(): void {
 * Display utilities
 ***********************************/
 function appearMenu(menuName: string): void {
-	document.getElementById("menu").classList.add(menuName + "Active");
+	let menu = document.getElementById("menu");
+	if (menu) {
+		menu.classList.add(menuName + "Active");
+	}
 	setTimeout(toggleOpacity, 100, menuName + "MenuItems", 1);
 	toggleDisplay(menuName + "MenuItems", true);
 }
 
 function disappearMenu(menuName: string): void {
-	document.getElementById("menu").classList.remove(menuName + "Active");
-	document.getElementById(menuName + "MenuItems").style.opacity = "0";
+	let menu = document.getElementById("menu");
+	if (menu) {
+		menu.classList.remove(menuName + "Active");
+	}
+	let menuItems = document.getElementById(menuName + "MenuItems");
+	if (menuItems) {
+		menuItems.style.opacity = "0";
+	}
 	setTimeout(toggleDisplay, 300, menuName + "MenuItems", 0);
 }
 
 function toggleDisplay(elemID: string, on: boolean): void {
+	let element = document.getElementById(elemID);
+	if (!element) {
+		return;
+	}
 	if (on) {
-		document.getElementById(elemID).style.display = "inline";
+		element.style.display = "inline";
 	}
 	else {
-		document.getElementById(elemID).style.display = "none";
+		element.style.display = "none";
 	}
 }
 
 function toggleOpacity(elemID: string, on: boolean): void {
+	let element = document.getElementById(elemID);
+	if (!element) {
+		return;
+	}
 	if (on) {
-		document.getElementById(elemID).style.opacity = "1";
+		element.style.opacity = "1";
 	}
 	else {
-		document.getElementById(elemID).style.opacity = "0";
+		element.style.opacity = "0";
 	}
 }

EuchreSolution/Euchre/GameScript/playerAPI.ts
@@ -38,11 +38,12 @@ function followsSuit(card: Card, trickSuit: Suit): boolean {
    order up a given suit.
    Depends on bidding round */
 function canOrderUpSuit(hand: Card[], suit: Suit): boolean {
+	let trumpCandidateCard = game.getTrumpCandidateCard() as Card;
 	if (game.getGameStage() === GameStage.BidRound1) {
-		if (game.getTrumpCandidateCard().suit !== suit) return false;
+		if (trumpCandidateCard.suit !== suit) return false;
 		if (hasSuit(hand, suit)) return true;
 	} else if (game.getGameStage() === GameStage.BidRound2) {
-		if (game.getTrumpCandidateCard().suit === suit) return false;
+		if (trumpCandidateCard.suit === suit) return false;
 		if (hasSuit(hand, suit)) return true;
 	}
 	return false;
@@ -61,7 +62,7 @@ function numCardsOfSuit(hand: Card[], suit: Suit): number {
 //**NOT TESTING**
 //number of suits you're holding
 function countSuits(): number {
-	let suitArray = [];
+	let suitArray: Suit[] = [];
 	let hand = myHand();
 	for (let i = 0; i < hand.length; i++) {
 		suitArray[hand[i].suit] = 1;
@@ -70,7 +71,7 @@ function countSuits(): number {
 }
 
 //**NOT TESTING**
-function getFirstLegalCard(hand: Card[], suitLead: Suit): Card {
+function getFirstLegalCard(hand: Card[], suitLead?: Suit): Card | undefined {
 	for (let i = 0; i < hand.length; i++) {
 		if (isValidPlay(hand, hand[i], suitLead)) {
 			return hand[i];
@@ -125,9 +126,12 @@ function greaterCard(card1: Card, card2: Card, trickSuit: Suit, trump: Suit): Ca
 }
 
 //**TESTED**
-function isValidPlay(hand: Card[], card: Card, trickSuit: Suit): boolean {
-	if (card == null) { //double equal will also find undefined
+function isValidPlay(hand: Card[], card?: Card, trickSuit?: Suit): boolean {
+	if (!card) {
 		return false;
+	}
+	if (!trickSuit) {
+		return true;
 	}
 	if (!hasSuit(hand, trickSuit)) {
 		return true;
@@ -158,8 +162,8 @@ function getCardValue(card: Card, trickSuit?: Suit, trump?: Suit): number {
 
 //**TESTED**
 //returns: the best card of the trick and who played it as a PlayedCard
-function getBestCardPlayed(cards: PlayedCard[], trump: Suit): PlayedCard {
-	if (cards.length === 0) return;
+function getBestCardPlayed(cards: PlayedCard[], trump: Suit): PlayedCard | null {
+	if (cards.length === 0) return null;
 	if (cards.length === 1) return cards[0];
 
 	let bestCard: Card = cards[0].card;
@@ -183,8 +187,8 @@ function getBestCardPlayed(cards: PlayedCard[], trump: Suit): PlayedCard {
 
 //**TESTED**
 //returns: the strongest card in your hand as a Card
-function getBestCardInHand(hand: Card[], trickSuit?: Suit, trump?: Suit): Card {
-	if (hand.length === 0) return;
+function getBestCardInHand(hand: Card[], trickSuit?: Suit, trump?: Suit): Card | null {
+	if (hand.length === 0) return null;
 	if (hand.length === 1) return hand[0];
 
 	let bestCard: Card = hand[0];

EuchreSolution/Euchre/GameScript/trick.ts
@@ -1,16 +1,16 @@
 ﻿class Trick {
-	private __suitLead: Suit = null; //the suit that was lead
+	private __suitLead: Suit | undefined = undefined; //the suit that was lead
 	private __playedCards: PlayedCard[] = []; //array of cards that have been played this trick so far
-	private __trumpSuit: Suit = null; //set in constructor
+	private __trumpSuit: Suit; //set in constructor
 	private __alone: boolean = false; //set in constructor
 	private __hands: Card[][];
-	private __aiPlayers: EuchreAI[];
+	private __aiPlayers: (EuchreAI | null)[];
 
 	/* Properties */
 	public playersPlayed(): number {
 		return this.__playedCards.length;
 	}
-	public suitLead(): Suit {
+	public suitLead(): Suit | undefined {
 		return this.__suitLead;
 	}
 	public cardsPlayed(): PlayedCard[] {
@@ -29,7 +29,7 @@
 	}
 
 	/* constructor */
-	constructor(trumpSuit: Suit, alone: boolean, hands: Card[][], aiPlayers: EuchreAI[]) {
+	constructor(trumpSuit: Suit, alone: boolean, hands: Card[][], aiPlayers: (EuchreAI | null)[]) {
 		this.__trumpSuit = trumpSuit;
 		this.__alone = alone;
 		this.__hands = hands;
@@ -43,7 +43,7 @@
 		if (this.isFinished()) return;
 
 		if (!isInHand(hand, card) || !isValidPlay(hand, card, this.__suitLead)) {
-			card = getFirstLegalCard(hand, this.__suitLead);
+			card = getFirstLegalCard(hand, this.__suitLead) as Card;
 		}
 		this.__playedCards.push({ player: player, card: card });
 
@@ -52,16 +52,21 @@
 
 	/* Public functions */
 	public playTrickStep(player: Player): void {
-		let card: Card;
+		let card: Card | null;
 		let hand: Card[] = this.__hands[player];
 
 		if (this.isFinished()) return;
 
+		let aiPlayer = this.__aiPlayers[player];
+		if (aiPlayer) {
+			card = aiPlayer.pickCard();
+		} else {
+			//TODO: implement this
+			card = null;
+		}
 
-		card = this.__aiPlayers[player].pickCard();
-
-		if (!isInHand(hand, card) || !isValidPlay(hand, card, this.__suitLead)) {
-			card = getFirstLegalCard(hand, this.__suitLead);
+		if (!card || !isInHand(hand, card) || !isValidPlay(hand, card, this.__suitLead)) {
+			card = getFirstLegalCard(hand, this.__suitLead) as Card;
 		}
 		this.playCard(player, card);
 	}
@@ -71,14 +76,21 @@
 		else return this.__playedCards.length >= 3;
 	}
 
-	public winningTeam(): Team {
-		let winningPlayer: Player;
+	public winningTeam(): Team | null {
+		let bestCardPlayed = getBestCardPlayed(this.__playedCards, this.__trumpSuit);
+		if (!bestCardPlayed) {
+			return null;
+		}
 
-		winningPlayer = getBestCardPlayed(this.__playedCards, this.__trumpSuit).player;
-		return getTeam(winningPlayer);
+		return getTeam(bestCardPlayed.player);
 	}
 
-	public winner(): Player {
-		return getBestCardPlayed(this.__playedCards, this.__trumpSuit).player;
+	public winner(): Player | null {
+		let bestCardPlayed = getBestCardPlayed(this.__playedCards, this.__trumpSuit);
+		if (!bestCardPlayed) {
+			return null;
+		}
+
+		return bestCardPlayed.player;
 	}
 }

commit 4a3e991f6c4cbf3e8eafc173304cf97f40ad0350

Fix an issue introduced in 722b04e55ded3dd9
The scoring worked as if everyone always went alone

EuchreSolution/Euchre/GameScript/animation.ts
@@ -273,6 +273,7 @@ function animWinTrick(player: Player, cards: Card[]): void {
 
 	for (let i = 0; i < 4; i++) {
 		if (cards[i] === null) {
+			//TODO: either mark the parameter as (Card | null)[], or remove this check
 			continue;
 		}
 		cardElem = document.getElementById(cards[i].id) as HTMLElement;

EuchreSolution/Euchre/GameScript/game.ts
@@ -391,7 +391,7 @@ class Game {
 
 	private startTricks(): void {
 		this.__gameStage = GameStage.PlayTricks;
-		this.__trick = new Trick(this.__trumpSuit as Suit, (this.__alonePlayer !== null), this.__hands, this.__aiPlayers);
+		this.__trick = new Trick(this.__trumpSuit as Suit, (this.__alonePlayer !== undefined), this.__hands, this.__aiPlayers);
 
 	}
 
@@ -408,7 +408,7 @@ class Game {
 		}
 		else {
 			this.__currentPlayer = this.__trick.winner() as Player;
-			this.__trick = new Trick(this.__trumpSuit as Suit, (this.__alonePlayer !== null), this.__hands, this.__aiPlayers);
+			this.__trick = new Trick(this.__trumpSuit as Suit, (this.__alonePlayer !== undefined), this.__hands, this.__aiPlayers);
 			this.__trickNum++;
 		}
 	}
@@ -448,7 +448,7 @@ class Game {
 
 	private updateScore(): void {
 		let isMaker;
-		let alone = (this.__alonePlayer !== null);
+		let alone = (this.__alonePlayer !== undefined);
 
 		if (this.__nsTricksWon > this.__ewTricksWon) {
 			isMaker = (this.__maker === Player.North || this.__maker === Player.South);